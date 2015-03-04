2c4d658847170d4ffb6814e96e9a656b4a979fb2
eta4ever <arlorlo@ya.ru>
2015-03-05 01:26:58 +0300

    Конфига на SD / Config on SD
    Теперь конфигурация берется из файла на SD-карте

--- main.ino
+++ main.ino
@@ -2,16 +2,33 @@
 #include <LWiFi.h> // для вайфайки
 #include <LWiFiUdp.h> // для вайфайки UDP
 
+// для работы с SD-картой
+#include <LTask.h>
+#include <LSD.h>
+#include <LStorage.h>
+
 //-------------------ДЛЯ WIFI NTP------------------------------------
-char ssid[] = "";  //  your network SSID (name)
-char pass[] = "";       // your network password
-unsigned int localPort = 2390;      // local port to listen for UDP packets
-IPAddress timeServer(129, 6, 15, 28); // time.nist.gov NTP server
-const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
-byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
-LWiFiUDP Udp; // A UDP instance to let us send and receive packets over UDP
+char ssid[] = "";  	// название сети, задается в конфиге на SD
+char pass[] = "";   // пароль сети, задается в конфиге на SD
+unsigned int localPort = 2390;      // локальный порт для NTP
+IPAddress timeServer; //  IP адрес NTP-сервера, задается в конфиге на SD
+const int NTP_PACKET_SIZE = 48; // время NTP в первых 48 байтах пакета
+byte packetBuffer[NTP_PACKET_SIZE]; // буфер вход. и выход. пакетов
+LWiFiUDP Udp; // экземпляр класса для работы с UDP
 //--------------------------------------------------------------------
 
+//------------------ДЛЯ РАБОТЫ С SD КАРТОЙ----------------------------
+#define Drv LSD // использовать SD карту, а не внутренний флеш
+LFile configFile; // экземпляр класса для работы с файлом конфига
+const char configPath[] = "/config/main.txt"; // путь к конфигу на SD
+//--------------------------------------------------------------------
+
+// тип данных конфигурации и переменная конфигурации
+typedef struct {int timezone;
+                String ssid;
+                String password;
+                IPAddress timeserver;} nixieConfigType;
+nixieConfigType nixieConfig;
 
 // пины для последовательного вывода времени и тактирования
 char nixieData = 2;
@@ -30,17 +47,103 @@ bool ntpSynced = false;
 
 void setup()
 {
+	
+	// Serial.begin(9600); // для дебага
+	// while(!Serial.available()); //для дебага
+
+	//------------ЗАГРУЖАЕМ КОНФИГУ С SD-------------------
+	pinMode(10, OUTPUT); // так в примере было, пока не трогаем
+	LTask.begin();
+    Drv.begin();
+
+    //открыть файл для чтения
+	configFile = Drv.open(configPath);
+    if (configFile) {
+    	// Serial.println("Loading config..."); //дебаг
+        // отправиться в начало
+        configFile.seek(0);
+        // и читать, пока файл не закончится
+        while (configFile.available()) {            
+            
+            //прочитать символ, добавить его к буферной строке
+            char symbol = configFile.read();
+            String fileString = "";
+            fileString += symbol;
+
+            // заполнить буферную строку
+            while ( (symbol != '\n') && configFile.available() )
+            {
+                // прочитать символ из файла
+                symbol = configFile.read();
+                // если символ не EOL, добавить его к строке
+                if (symbol != '\n') fileString += symbol; 
+            }
+
+            // выделить из строки имя и значение параметра
+            String paramName = getParamName(fileString);
+            String paramValue = getParamValue(fileString);
+
+            // поскольку мы не можем юзать switch по стрингам, будет
+            // такое вот if-else. Это заполнение структуры
+            // конфигурации системы
+
+            if (paramName == "timezone") 
+            {
+                nixieConfig.timezone = paramValue.toInt();
+            }
+            else if (paramName == "ssid")
+            {
+                nixieConfig.ssid=paramValue;
+            }
+            else if (paramName == "password")
+            {
+                nixieConfig.password=paramValue;
+            }
+            else if (paramName == "timeserver")
+            {
+                nixieConfig.timeserver = stringToIP(paramValue);
+            }
+
+        }
+        // закрыть файл:
+        configFile.close();
+    } else {
+    	// в случае проблемы с файлом не делаем ничего
+    	// Serial.println("Loading config FAILED"); //дебаг
+    }
+
+    // Serial.println(nixieConfig.timezone);
+    // Serial.println(nixieConfig.ssid);
+    // Serial.println(nixieConfig.password);
+    // Serial.println(nixieConfig.timeserver);
+
 	//-------------WIFI------------------------------------
-	// attempt to connect to Wifi network:
+	// подключиться к сети:
 	LWiFi.begin();
-	while (!LWiFi.connectWPA(ssid, pass))
+
+	// Тут вот какая штука. LWiFi.connectWPA String не хочет, ему
+	// char подавай. А сделать char сразу в структуре не получилось,
+	// т.к. была какая-то мутная бяка, связанная с длиной cjar в структуре
+	// и длиной буфера при конвертации из String при разборке файла.
+	// Так что, в структуре String, а здесь конвертим, точно зная,
+	// сколько нужно символов.
+
+	char ssid [nixieConfig.ssid.length()];
+	char password [nixieConfig.password.length()];
+	nixieConfig.ssid.toCharArray(ssid,nixieConfig.ssid.length());
+	nixieConfig.password.toCharArray(password,nixieConfig.password.length());
+
+	// Serial.println(ssid);
+	// Serial.println(password);
+
+	while (!LWiFi.connectWPA(ssid, password))
 	{
 	  delay(1000);
+	  // Serial.println("Trying Wi-Fi...");
 	}
     delay(10000);
 	Udp.begin(localPort);
 	//----------------------------------------------------
-
 
 	// установить режимы выходов
 	pinMode(nixieData, OUTPUT);
@@ -54,10 +157,11 @@ void setup()
         LDateTime.setTime(&currentTime);
         outputTime(currentTime);
 
-        // начальная NTP-синхронизация, долбимся до победного
+        // начальная NTP-синхронизация, долбиться до победного
         bool sync_success = NTPsync();
         while (!sync_success){
         	delay(10000);
+        	// Serial.println("Trying NTP...");
         	sync_success = NTPsync();
         }
 
@@ -191,7 +295,7 @@ unsigned long sendNTPpacket(IPAddress& address)
 bool NTPsync(){
 
   bool result = false;
-  sendNTPpacket(timeServer); // отправить NTP пакет 
+  sendNTPpacket(nixieConfig.timeserver); // отправить NTP пакет 
   // подождать ответа
   delay(1000);
   if ( Udp.parsePacket() ) // если получен ответный пакет
@@ -200,23 +304,61 @@ bool NTPsync(){
     Udp.read(packetBuffer, NTP_PACKET_SIZE); // прочитать пакет в буфер
 
     // timestamp - начинается с 40 байта пакета, длина 4 байта (2 слова)
-    // сначала читаем эти два слова 
+    // сначала читать эти два слова 
     unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
     unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
-    // потом объединяем в long и получаем секунды с 1 января 1900
+    // потом объединить в long, получаются секунды с 1 января 1900
     unsigned long secsSince1900 = highWord << 16 | lowWord;
-    // Unix time - это секунды с 1 января 1970. Т.е., для получения UnixTime вычитаем 70 лет
+    // Unix time - это секунды с 1 января 1970. Т.е., 
+    // для получения UnixTime вычесть 70 лет
     const unsigned long seventyYears = 2208988800UL;
     unsigned long epoch = secsSince1900 - seventyYears;
-    // и вытаскиваем из Unix Time часы и минуты с поправкой на часовой пояс,
+    // и вытащить из Unix Time часы и минуты с поправкой на часовой пояс,
     // с коррекцией перехода через полночь при прибавлении пояса (%24)
     currentTime.hour = ((epoch  % 86400L) / 3600 + timezone) % 24; // (86400 секунд в дне)
     currentTime.min = (epoch  % 3600) / 60; // (3600 секунд в минуте)
     
-    LDateTime.setTime(&currentTime); // устанавливаем время
+    LDateTime.setTime(&currentTime); // установить время
 
     digitCycle(); // эффект прокрутки циферок
     result = true; // флаг обработанного пакета
   }
   return result;
+}
+
+//--------------ВСПОМОГАТЕЛЬНЫЕ ФУНКЦИИ ДЛЯ ЗАГРУЗКИ КОНФИГУРАЦИИ------
+
+// выделить из строки имя параметра (до разделителя)
+String getParamName(String source)
+{
+    char delimiter = '=';
+    // найти первый разделитель, считать его единственным
+    int delimiterPos = source.indexOf(delimiter);
+    // обрезать строку 
+    return (source.substring(0,delimiterPos));
+}
+
+// выделить из строки значение параметра (после разделителя)
+String getParamValue(String source)
+{
+    char delimiter = '=';
+    // найти первый разделитель, считать его единственным
+    int delimiterPos = source.indexOf(delimiter);
+    // обрезать строку 
+    return (source.substring(delimiterPos+1));
+}
+
+//преобразовать строку вида 192.168.0.1 в IPAddress
+IPAddress stringToIP(String source)
+{
+    String oct[4]; // 4 октета
+    int firstDotPos = source.indexOf('.'); // позиция первой точки слева
+    oct[3] = source.substring(0,firstDotPos); // старший (левый) октет
+    int secondDotPos = source.indexOf('.', firstDotPos+1); // позиция второй точки
+    oct[2] = source.substring(firstDotPos+1, secondDotPos); // второй октет
+    int thirdDotPos = source.indexOf('.', secondDotPos+1); // позиция третьей точки
+    oct[1] = source.substring(secondDotPos+1, thirdDotPos); // первый октет
+    oct[0] = source.substring(thirdDotPos+1); // младший (правый) октет
+
+    return IPAddress(oct[3].toInt(), oct[2].toInt(), oct[1].toInt(), oct[0].toInt());
 }
